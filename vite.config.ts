fcdc5ac9345fa16b96eaca04e25d15d041d09932
Eduardo Nunes <eduardonunes2525@gmail.com>
2022-09-14 17:04:16 -0300

    feat: Adds SVG plugin to Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), svgr()],
   server: {
     port: 3000,
   },
