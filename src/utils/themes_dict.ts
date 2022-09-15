ff32a83352e15531d08ac285f6a7cd935d67d376
Eduardo Nunes <eduardonunes2525@gmail.com>
2022-09-15 02:33:06 -0300

    style: Add themes icons and dict constants

---
+++ src/utils/themes_dict.ts
@@ -0,0 +1,11 @@
+import sith from '../styles/themes/sith';
+import jedi from '../styles/themes/jedi';
+import empire from '../styles/themes/empire';
+import alliance from '../styles/themes/alliance';
+
+export const THEMES = {
+  sith: sith,
+  jedi: jedi,
+  empire: empire,
+  alliance: alliance,
+};
---
+++ src/utils/themes_icons.tsx
@@ -0,0 +1,11 @@
+import { ReactComponent as JediLogo } from '../../assets/icons/jedi_logo.svg';
+import { ReactComponent as SithLogo } from '../../assets/icons/sith_logo.svg';
+import { ReactComponent as EmpireLogo } from '../../assets/icons/empire_logo.svg';
+import { ReactComponent as AllianceLogo } from '../../assets/icons/alliance_logo.svg';
+
+export const THEMES_ICONS = {
+  sith: <SithLogo />,
+  jedi: <JediLogo />,
+  empire: <EmpireLogo />,
+  alliance: <AllianceLogo />,
+};
