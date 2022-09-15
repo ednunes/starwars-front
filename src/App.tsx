e666d1ec0305534131eb2c9d4ed0884609dcc97d
Eduardo Nunes <eduardonunes2525@gmail.com>
2022-09-15 02:30:49 -0300

    feat: Add change theme method into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import { ThemeProvider } from 'styled-components';
-import Header from './components/Header';
 import GlobalStyle from './styles/global';
-import dark from './styles/themes/dark';
+import Header from './components/Header';
+import usePersistedState from './utils/usePersistedState';
+import { THEMES } from './utils/themes_dict';
 
 function App() {
+  const [theme, setTheme] = usePersistedState('theme', 'sith');
+
+  const handleSetTheme = () => {
+    setTheme((theme: string) => {
+      const themes_keys = Object.keys(THEMES);
+      const next_theme_index = themes_keys.findIndex((t) => t === theme) + 1;
+      const new_theme =
+        next_theme_index > themes_keys.length - 1 ? themes_keys[0] : themes_keys[next_theme_index];
+
+      return new_theme;
+    });
+  };
+
   return (
-    <ThemeProvider theme={dark}>
+    <ThemeProvider theme={THEMES[theme]}>
       <GlobalStyle />
-      <Header />
+      <Header handleSetTheme={handleSetTheme} theme={theme} />
     </ThemeProvider>
   );
 }
