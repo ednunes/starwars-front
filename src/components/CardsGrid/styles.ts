3921b2729af05bc34687d5f69ce6727eaf022def
Eduardo Nunes <eduardonunes2525@gmail.com>
2022-09-23 01:09:39 -0300

    refactor: Change structure of Card contents

--- src/components/CardsGrid/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Grid = styled.div`
-  margin-top: 40px;
-  display: grid;
-  gap: 2rem;
-  place-items: center;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-`;
