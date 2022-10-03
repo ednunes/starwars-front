6717b71fbc31b591b36856c4e716b57b82231fe9
Eduardo Nunes <eduardonunes2525@gmail.com>
2022-10-03 17:40:21 -0300

    fix: Fix types of functions and parameters of Button and CardList components

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,6 @@
 import * as S from './styles';
 
-function Button({ text, handleClick }: { text: string; handleClick: () => void }) {
+function Button({ text, handleClick }: { text: JSX.Element; handleClick: () => void }) {
   return <S.Button onClick={handleClick}>{text}</S.Button>;
 }
 
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,4 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 import { device } from '~/utils/device_sizes';
 
 export const Header = styled.header`
