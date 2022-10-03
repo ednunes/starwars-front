1d7cb1b33dfa8ec68d15047fbef431e342d6b7b8
Eduardo Nunes <eduardonunes2525@gmail.com>
2022-10-03 17:42:23 -0300

    production: Version: 0.1

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  base: '/starwars-front/',
   plugins: [react(), tsconfigPaths(), svgr()],
   server: {
     port: 3000,
