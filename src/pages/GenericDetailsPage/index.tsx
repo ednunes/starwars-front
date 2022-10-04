4e990445adb9049b1c60da6b1444a6bdc55d9dd4
Eduardo Nunes <eduardonunes2525@gmail.com>
2022-10-04 13:41:54 -0300

    feat: Creates a generic details page component

---
+++ src/pages/GenericDetailsPage/index.tsx
@@ -0,0 +1,45 @@
+import { useParams } from 'react-router-dom';
+import * as Global from '~/components/StyledComponents/styles';
+import useFetch from '~/hooks/useFetch';
+import Loading from '~/components/Loading';
+import Translator from '~/utils/Translator';
+import * as S from './styles';
+import { Planet, PlanetRequest } from '../Planet/types';
+
+export default function GenericDetailsPage({
+  resource,
+  resourceId,
+  getContent,
+  formatData,
+}: {
+  resource: string;
+  resourceId: string;
+  getContent: (data: any) => JSX.Element;
+  formatData: (planet: PlanetRequest) => Promise<Planet>;
+}) {
+  const params = useParams();
+  const [{ data, isLoading }]: any = useFetch(`${resource}/${params[resourceId]}`, [], formatData);
+
+  return isLoading ? (
+    <Global.CentralizedComponent>
+      <Loading />
+    </Global.CentralizedComponent>
+  ) : (
+    <>
+      <Global.TitleContainer>
+        <S.Column>
+          <Global.Title>{data.name}</Global.Title>
+        </S.Column>
+        <S.Column>
+          <Global.TitleDescription>
+            <Translator>generical.edited_at</Translator>: {data.edited}
+          </Global.TitleDescription>
+          <Global.TitleDescription>
+            <Translator>generical.created_at</Translator>: {data.created}
+          </Global.TitleDescription>
+        </S.Column>
+      </Global.TitleContainer>
+      {getContent(data)}
+    </>
+  );
+}
---
+++ src/pages/GenericDetailsPage/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { device } from '~/utils/device_sizes';
+
+export const Column = styled.span`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  max-height: auto;
+
+  @media ${device.laptop} {
+    max-height: 380px;
+  }
+`;
