567a06635051e4eec6a1d12485bb64362dd80dc5
Eduardo Nunes <eduardonunes2525@gmail.com>
2022-10-04 15:31:14 -0300

    style: Delete movie chart card style file

--- src/components/MovieChartCard/index.tsx
+++ src/components/MovieChartCard/index.tsx
@@ -2,15 +2,14 @@ import Translator from '~/utils/Translator';
 import * as Global from '~/components/StyledComponents/styles';
 import { getList } from '~/utils/utils';
 import { MovieLink } from '~/utils/types';
-import * as S from './styles';
 
 export default function MovieChartCard({ movies }: { movies: MovieLink[] }) {
   return (
-    <S.BasicInfo>
+    <Global.BasicCard>
       <Global.Subtitle>
         <Translator>pages.movies</Translator>
       </Global.Subtitle>
       {getList(movies, 'movies', 'title')}
-    </S.BasicInfo>
+    </Global.BasicCard>
   );
 }
--- src/components/MovieChartCard/styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-import { Card } from '~/components/StyledComponents/styles';
-
-export const BasicInfo = styled(Card)`
-  width: 100%;
-`;
